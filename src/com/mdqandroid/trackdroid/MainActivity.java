95c02b1658e61899b9f7e1d689a42d9a94427637
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-11-03 00:28:57 -0300

    cambio en la interfaz

--- src/com/mdqandroid/trackdroid/MainActivity.java
+++ src/com/mdqandroid/trackdroid/MainActivity.java
@@ -4,18 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+	Button btn_Enviar ;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LevantarXML();
     }
 
 
-    @Override
+    private void LevantarXML() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
