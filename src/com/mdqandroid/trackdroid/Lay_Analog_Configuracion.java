6f961e0a2bcdf046ae830c1cd742e0baae98ff7f
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-11-12 12:36:12 -0300

    cmabios

---
+++ src/com/mdqandroid/trackdroid/Lay_Analog_Configuracion.java
@@ -0,0 +1,21 @@
+package com.mdqandroid.trackdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lay_Analog_Configuracion  extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lay_analog_configuracion);
+		LevantarXML();
+		
+	}
+
+	private void LevantarXML() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
